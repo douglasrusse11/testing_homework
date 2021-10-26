709ea77236b6463759bc6bf765325732a66a64d2
Douglas Russell <douglas.russell7@btinternet.com>
2021-10-26 17:10:30 +0100

    adds test_add

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,4 +2,5 @@ import unittest
 from src.calculator import add, divide, multiply, subtract
 
 class TestCalculator(unittest.TestCase):
-    pass
+    def test_add(self):
+        self.assertEqual(5, add(2, 3))
